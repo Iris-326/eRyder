f4ee7d9b380010199d8ef821d12fa7c4a27a99dc
wangziyan <3420278992@qq.com>
2026-03-10 19:42:31 +0800

    Submit exercise 4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args)  {
-        ERyder bike1 = new ERyder();
+        ERyder bike1 = new ERyder("001", 85, true, 10, "user123", "1234567890");
         bike1.printBikeDetails();
+        bike1.printRideDetails();
+        bike1.ride();
+        System.out.println("Calculating fare for bike1: " + bike1.calculateFare(30) + " dollars");
 
-        ERyder bike2 = new ERyder("2007", 32, true, 6);
+
+        ERyder bike2 = new ERyder("002", 32, true, 6, "user456", "0987654321");
         bike2.printBikeDetails();
+        bike2.printRideDetails();
         bike2.ride();
-        bike2.printBikeDetails();
+        System.out.println("Calculating fare for bike2: " + bike2.calculateFare(30) + " dollars");
+    
     }
+        
+
+        
+      
 }
